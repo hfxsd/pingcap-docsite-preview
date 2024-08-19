1d738b4e3046e66cfebb6d0fb295c1977e0f0c26
houfaxin <hfxsd@hotmail.com>
2024-08-19 13:22:49 +0800

    Update sync_mult_prs.sh

--- sync_mult_prs.sh
+++ sync_mult_prs.sh
@@ -15,8 +15,8 @@ cd "$SCRIPT_DIR"
 # The PRs will be synced in the order of the following statements.
 ./sync_pr.sh preview/pingcap/docs/18386
 ./sync_pr.sh preview/pingcap/docs-cn/18095
-./sync_pr.sh preview-cloud/pingcap/docs/"$CLOUD_DOCS_PR"
-./sync_pr.sh preview-operator/pingcap/docs-tidb-operator/"$OPERATOR_DOCS_PR"
+# ./sync_pr.sh preview-cloud/pingcap/docs/"$CLOUD_DOCS_PR"
+# ./sync_pr.sh preview-operator/pingcap/docs-tidb-operator/"$OPERATOR_DOCS_PR"
 
 # Synchronize the content from master to release-x.y directories.
 rsync -av markdown-pages/zh/tidb/master/ markdown-pages/zh/tidb/"$RELEASE_DIR"/

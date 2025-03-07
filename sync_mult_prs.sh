22a990fbc1a97c6131b9b9e314e7a7d39a3c454a
houfaxin <hfxsd@hotmail.com>
2025-03-07 14:36:53 +0800

    Update sync_mult_prs.sh

--- sync_mult_prs.sh
+++ sync_mult_prs.sh
@@ -13,8 +13,8 @@ cd "$SCRIPT_DIR"
 
 # Define the PRs to sync.
 # The PRs will be synced in the order of the following statements.
-./sync_pr.sh preview/pingcap/docs/"https://github.com/pingcap/docs/pull/20463"
-./sync_pr.sh preview/pingcap/docs-cn/"https://github.com/pingcap/docs-cn/pull/19962"
+./sync_pr.sh preview/pingcap/docs/20463
+./sync_pr.sh preview/pingcap/docs-cn/19962
 # ./sync_pr.sh preview-cloud/pingcap/docs/"$CLOUD_DOCS_PR"
 # ./sync_pr.sh preview-operator/pingcap/docs-tidb-operator/"$OPERATOR_DOCS_PR"
 
